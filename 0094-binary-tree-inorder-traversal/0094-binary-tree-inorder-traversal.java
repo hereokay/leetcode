ddfd3ac0ba1e37982c5a88ea6374274b05a43645
hereokay <hereokay88@gmail.com>
2023-11-30 21:43:11 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (44.28%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,18 @@
+class Solution {
+    static List<Integer> list;
+    public List<Integer> inorderTraversal(TreeNode root) {
+        list = new ArrayList<>();
+        inorder(root);
+        return list;
+    }
+
+    public void inorder(TreeNode root){
+        if (root==null){
+            return;
+        }
+
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
